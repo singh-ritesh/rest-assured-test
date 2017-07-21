849302ad58509f9ac2f4865b8ff8fc191cb2ea2f
singh-ritesh <ritesh@yahoo.com>
2017-07-21 13:50:28 +0530

    project files

---
+++ src/main/java/test/rest_assured_test/App.java
@@ -0,0 +1,13 @@
+package test.rest_assured_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
